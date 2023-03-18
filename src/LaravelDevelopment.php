6c36ee21090c58a589d3e8286fa99fbf26a81511
Francisco Kraefft <fkraefft@ivirtual.la>
2023-03-18 16:35:06 -0300

    Horizon check added.

--- src/LaravelDevelopment.php
+++ src/LaravelDevelopment.php
@@ -15,6 +15,7 @@ use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
 use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
 use Spatie\Health\Checks\Checks\RedisCheck;
 use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
@@ -65,6 +66,8 @@ class LaravelDevelopment
             ...$this->getDatabaseChecks(),
 
             ...$this->getRedisChecks(),
+
+            HorizonCheck::new(),
         ]);
     }
 
