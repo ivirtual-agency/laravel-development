bc07b914382b7d84cbfa5518f495db794a802965
Francisco Kraefft <fkraefft@ivirtual.la>
2023-03-18 13:38:42 -0300

    Flare check added.

---
+++ src/Checks/FlareCheck.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace iVirtual\LaravelDevelopment\Checks;
+
+use Encodia\Health\Checks\EnvVars;
+use Spatie\Health\Checks\Result;
+
+class FlareCheck extends EnvVars
+{
+    /**
+     * Check if Spatie Ignition (Flare) is installed and the correctly configured.
+     */
+    public function run(): Result
+    {
+        if (! $this->isFlareInLoggingChannel()) {
+            return Result::make()
+                ->failed('Flare is not in the logging channel.')
+                ->shortSummary('Not installed');
+        }
+
+        // Check for Flare key.
+        $this->requireVars(['FLARE_KEY']);
+
+        return parent::run();
+    }
+
+    /**
+     * Check if flare is in the logging channels.
+     */
+    private function isFlareInLoggingChannel(): bool
+    {
+        // Check if logging is set to flare.
+        if (config('logging.default') === 'flare') {
+            return true;
+        }
+
+        // Check if logging is set to stack and flare is in channels.
+        return config('logging.default') === 'stack'
+            && in_array(
+                'flare',
+                config('logging.channels.stack.channels', [])
+            );
+    }
+}
