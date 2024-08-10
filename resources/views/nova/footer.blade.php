a28d5ea420020eb4e7dc166a33bdfa55c0bfb5f5
Francisco Kraefft <fkraefft@ivirtual.la>
2024-08-10 13:02:45 -0300

    iVirtual url updated to the new one.

--- resources/views/nova/footer.blade.php
+++ resources/views/nova/footer.blade.php
@@ -1,5 +1,5 @@
 <p class="text-center">
-    <span>@lang('Developed by') <a class="link-default" href="https://ivirtual.la?utm_source=laravel_nova&utm_medium=footer" target="_blank">iVirtual</a></span>
+    <span>@lang('Developed by') <a class="link-default" href="https://ivirtual.agency?utm_source=laravel_nova&utm_medium=footer" target="_blank">iVirtual</a></span>
     <span> · </span>
     <span>v{!! $version !!}</span>
 </p>
