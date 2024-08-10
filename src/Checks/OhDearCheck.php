c8156ef4ce3b6cef8edb0e90af53389a6f7ec715
Francisco Kraefft <fkraefft@ivirtual.la>
2024-08-10 13:22:27 -0300

    Oh dear check updated.

--- src/Checks/OhDearCheck.php
+++ src/Checks/OhDearCheck.php
@@ -2,7 +2,6 @@
 
 namespace iVirtual\LaravelDevelopment\Checks;
 
-use Composer\InstalledVersions;
 use Spatie\Health\Checks\Check;
 use Spatie\Health\Checks\Result;
 
@@ -20,44 +19,13 @@ class OhDearCheck extends Check
         }
 
         if (! config('schedule-monitor.oh_dear.api_token')) {
-            return Result::make()
-                ->failed('Oh dear api token not added.');
+            return Result::make()->failed('Oh dear api token not added.');
         }
 
-        if (! $this->hasLaravelHorizonInstalled()) {
-            return Result::make()
-                ->ok('Ok')
-                ->shortSummary('Horizon not installed');;
-        }
-
-        $schedulerQueue = config('schedule-monitor.oh_dear.queue');
-
-        if ($schedulerQueue !== 'OH_DEAR') {
-            return Result::make()
-                ->meta([
-                    'actual' => $schedulerQueue,
-                    'expexted' => 'OH_DEAR',
-                ])
-                ->failed('Schedule monitor oh dear queue is not correcty set.')
-                ->shortSummary($schedulerQueue ?? '-');
-        }
-
-        if (! in_array(
-            'OH_DEAR',
-            config('horizon.defaults.supervisor-1.queue', [])
-        )) {
-            return Result::make()
-                ->failed('Horizon queue does not have "OH_DEAR" added.');
+        if (! config('health.oh_dear_endpoint.secret')) {
+            return Result::make()->failed('Oh dear endpoint secret not added.');
         }
 
         return Result::make()->ok('Ok');
-    }
-
-    /**
-     * Check if the project has laravel horizon installed.
-     */
-    private function hasLaravelHorizonInstalled(): bool
-    {
-        return InstalledVersions::isInstalled('laravel/horizon');
     }
 }
